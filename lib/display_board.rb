8db2c3e794c0bace551b6ac35f8aab678a75e9a0
Heather Wilson <hdaniwilson@gmail.com>
2021-04-02 00:31:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,5 +6,5 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
-board= [" ", "X", " ", " ", " ", " ", " ", " ", " "]
+board= [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 display_board(board)
